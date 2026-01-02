292405d6caaa435cec277ed9a9e984d739893e22
Komal-Chame <chandrakalachame123@gmail.com>
2026-01-02 22:18:38 +0530

    Demonstrated Code Of 2D Array That Taking Input and Print It

---
+++ Day05_2D_Array/Print2DArray.java
@@ -0,0 +1,48 @@
+//Create Java program to input and print 2D array using Scanner
+
+import java.util.Scanner;
+
+class Print2DArray
+{
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+
+		System.out.print("Enter Number of Rows : ");
+		int row = sc.nextInt();
+
+		System.out.println();
+
+		System.out.print("Enter Number of Columns : ");
+		int column = sc.nextInt();
+
+		int arr[][] = new int[row][column];
+
+		System.out.println();
+
+		System.out.println("Enter 2D Array Elements : ");
+
+		for(int i=0; i<row; i++)
+		{
+			for(int j=0; j<column; j++)
+			{
+				arr[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println();
+
+		System.out.println("2D Array Elements Are : ");
+
+		for(int i=0; i<row; i++)
+		{
+			for(int j=0; j<column; j++)
+			{
+				System.out.print(arr[i][j]+"\t");
+			}
+
+			System.out.println();
+		}
+	}
+}
