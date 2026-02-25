1c928857d8db7cdd177466d73c17be45ea4a2162
Komal <kic72762@gmail.com>
2026-02-25 22:48:51 +0530

    Added try-catch inside loop to handle exception values in arrayto handle NullPointerException

---
+++ Day16_ExceptionHandling/01_TryCatch/NullPointerEx1.java
@@ -0,0 +1,29 @@
+class NullPointerEx1
+{
+	public static void main(String[] args) {
+		
+		/*String names[] = {"Sakshi", null, "Aditi", "Komal", "Shraddha", "Prachi"};
+
+			for(int i = 0; i < names.length; i++)
+			{
+				System.out.println(names[i].length()); //i =1 it gives nullpointerException
+			}
+		*/
+
+		String names[] = {"Komal","Pratiksha",null,"Mohini","Aarti","Radhika"};
+
+		for(int i=0; i<names.length; i++)
+		{
+			try
+			{
+				System.out.println(names[i].length());
+			}
+			catch(NullPointerException e)
+			{
+				System.out.println("One name is null");
+			}
+		}
+
+		System.out.println("Program Continues....");
+	}
+}
