496f695ef2e3121ba8a781b98a5108f372e2b54e
Komal <kic72762@gmail.com>
2026-02-27 22:31:38 +0530

    Added the Example that Demonstrating Finally Execution with return statement

---
+++ Day16_ExceptionHandling/03_FinallyBlock/FinallyExample3.java
@@ -0,0 +1,25 @@
+class FinallyExample3
+{
+	public static void main(String[] args) {
+		
+		System.out.println(check());
+	}
+
+	static int check()
+	{
+		try
+		{
+			System.out.println("Inside try block");
+			return 100; //Returning value from try block
+		}
+
+		finally
+		{
+			// This block executes even before returning the value
+			System.out.println("Finally Block Executed Before Returning Value");
+		}
+
+		/* Even though return statement is inside try block,
+		finally block executes first and then the value is returned. */
+	}
+}
