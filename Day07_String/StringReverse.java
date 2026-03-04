07409c7ed13a7105ea0bf4e0aecd93bc7768286d
Komal <kic72762@gmail.com>
2026-03-04 22:01:43 +0530

    Demonstrated the code of String Reverse without using inbult method

---
+++ Day07_String/StringReverse.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class StringReverse
+{
+
+	static String reverse(String str)
+	{
+		String revStr = " ";
+
+		for(int i = str.length() - 1; i>=0; i--)
+		{
+			revStr = revStr + str.charAt(i);
+		}
+
+		return revStr;
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("\nEnter any String : ");
+		String str = sc.nextLine();
+
+		System.out.println("\nReverse : "+ reverse(str));
+	}
+}
