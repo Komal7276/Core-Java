819ffde55f114e5a4898b01ca28f1eb7223d507c
Komal <kic72762@gmail.com>
2026-03-11 23:43:48 +0530

    Demonstrated the Program of Java that Performs Divion Operation on Matrix and Calculates the Division Result

---
+++ Day05_2D_Array/MatrixDivision.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+class MatrixDivision
+{
+	public static void main(String[] args) {
+			
+			Scanner sc =new Scanner(System.in);
+
+			int a[][] = new int[2][2];
+			int b[][] = new int[2][2];
+
+			int div[][] = new int[2][2];
+
+			System.out.println("\nEnter First Matrix : ");
+
+			for(int i = 0; i < 2; i++)
+			{
+				for( int j = 0; j < 2; j++)
+				{
+					a[i][j] = sc.nextInt();
+				}
+			}
+
+			System.out.println("\nEnter Second Matrix : ");
+			for(int i = 0; i < 2; i++)
+			{
+				for( int j = 0; j < 2; j++)
+				{
+					b[i][j] = sc.nextInt();
+				}
+			}
+
+			for(int  i = 0; i < 2; i++)
+			{
+				for(int j = 0; j < 2; j++)
+				{
+					div[i][j] = a[i][j] / b[i][j];
+				}
+			}
+			
+
+			System.out.println("\nMatrix Devision Result : \t");
+
+			for(int i= 0; i < 2; i++)
+			{
+				for(int j = 0; j < 2; j++)
+				{
+					System.out.print("\t" + div[i][j] + " \t");
+				}
+				System.out.println();
+			}
+		}	
+
+
+}
