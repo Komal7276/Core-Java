b135fa58fc93f0d5181604019347ad1509eac7b0
Komal <kic72762@gmail.com>
2026-03-11 23:42:32 +0530

    Added the Java Program that Calculates Multiplication of Matrix

---
+++ Day05_2D_Array/MatrixMultiplication.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+class MatrixMultiplication
+{
+	public static void main(String[] args) {
+			
+			Scanner sc =new Scanner(System.in);
+
+			int a[][] = new int[2][2];
+			int b[][] = new int[2][2];
+
+			int mul[][] = new int[2][2];
+
+			System.out.println("\nEnter First Matrix : ");
+
+			for(int i = 0; i < 2; i++)
+			{
+				for( int j = 0; j < 2; j++)
+				{
+					a[i][j] = sc.nextInt();
+				}
+			}
+
+			System.out.println("\nEnter Second Matrix : ");
+
+
+			for(int i = 0; i < 2; i++)
+			{
+				for( int j = 0; j < 2; j++)
+				{
+					b[i][j] = sc.nextInt();
+				}
+			}
+
+			for(int  i = 0; i < 2; i++)
+			{
+				for(int j = 0; j < 2; j++)
+				{
+					mul[i][j] = a[i][j] * b[i][j];
+				}
+			}
+			
+
+			System.out.println("\nMatrix Multiplication Result : \t");
+
+			for(int i= 0; i < 2; i++)
+			{
+				for(int j = 0; j < 2; j++)
+				{
+					System.out.print("\t" + mul[i][j] + " \t");
+				}
+				System.out.println();
+			}
+		}	
+
+
+}
