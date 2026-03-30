3d5d4713a03457a8759b72bade91776c76b6bc46
Komal <kic72762@gmail.com>
2026-03-30 22:25:07 +0530

    Added the Java Pattern Code of Printing the Descending Number Pattern

---
+++ Day17_Pattern/Pattern11.java
@@ -0,0 +1,21 @@
+/*
+	5 4 3 2 1
+	4 3 2 1
+	3 2 1
+*/
+
+class Pattern11
+{
+	public static void main(String[] args) {
+		
+		for(int i=5; i>=3; i--)
+		{
+			for(int j=i; j>=1; j--)
+			{
+				System.out.print(j + " ");
+			}
+			
+			System.out.println();
+		}
+	}
+}
